2aaa32c88dd035f400ad40c3599447c9588ae3f6
UnidentifiedRaccoon <yura.posledov@yandex.ru>
2019-09-28 12:25:03 +0300

    eslint

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
+'use strict';
+
+//  Список констант
 var AVATAR_NUMBERS = ['01', '02', '03', '04', '05', '06', '07', '08'];
 var TITLES = ['Заголовок №1', 'Заголовок №2', 'Заголовок №3', 'Заголовок №4', 'Заголовок №5', 'Заголовок №6', 'Заголовок №7', 'Заголовок №8'];
-var ADDRESS = [];
-var LOCATIONS = [];
 var PRICES = ['1000', '2000', '3000', '4000'];
 var ROOMS = ['1', '2', '3', '4'];
 var GUESTS = ['1', '2', '3', '4', '5', '6'];
-var TYPES = ['palace',  'flat', 'house', 'bungalo'];
+var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var CHECKIN_ARR = ['12:00', '13:00', '14:00'];
 var CHECKOUT_ARR = ['12:00', '13:00', '14:00'];
-var  DESCRIPTIONS = ['Описание №1', 'Описание №2', 'Описание №3', 'Описание №4', 'Описание №5', 'Описание №6', 'Описание №7', 'Описание №8'];
+var DESCRIPTIONS = ['Описание №1', 'Описание №2', 'Описание №3', 'Описание №4', 'Описание №5', 'Описание №6', 'Описание №7', 'Описание №8'];
 var PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
@@ -27,9 +28,9 @@ var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map_
 
 
 // Функция для получение СЛУЧАЙНОЙ ЦИФРЫ в указанном диапазоне
-var getRandomNumber = function(min, max) {
+var getRandomNumber = function (min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
-}
+};
 
 // Функция для получения СЛУЧАЙНОГО ЭЛЕМЕНТА из массива
 // Прим: так как величина длинны массива больше величины индекса последнего элемента массива, Math.random() никогда не выдает один, а Math.floor() округляет вниз
@@ -40,34 +41,34 @@ var getRandomArrayElement = function (arr) {
 };
 
 // Функция для изменения длинны массива
-var changeArrayLength = function(arr) {
+var changeArrayLength = function (arr) {
   var changedArr = arr;
   changedArr.length = getRandomNumber(0, arr.length);
   return changedArr;
-}
+};
 
 // Функция возращаюшая обьект мага с рандомными свойствами, заданными на основании данных массивов
-var getAnnouncementObject = function (avatar_number, title, prices, types, rooms, guests, checkin_arr, checkout_arr, features, descriptions, photos) {
+var getAnnouncementObject = function (avatarNumber, title, prices, types, rooms, guests, checkinArr, checkoutArr, features, descriptions, photos) {
   var announcement = {
-    "author": {
-      "avatar": 'img/avatars/user' + avatar_number + '.png',
+    'author': {
+      'avatar': 'img/avatars/user' + avatarNumber + '.png',
     },
-    "offer": {
-      "title": title,
-      "address": this.location.x + ', ' + this.location.y,
-      "price": getRandomArrayElement(prices),
-      "type": getRandomArrayElement(types),
-      "rooms": getRandomArrayElement(rooms),
-      "guests": getRandomArrayElement(guests),
-      "checkin": getRandomArrayElement(checkin_arr),
-      "checkout": getRandomArrayElement(checkout_arr),
-      "features": changeArrayLength(features),
-      "description": getRandomArrayElement(descriptions),
-      "photos": changeArrayLength(photos),
+    'offer': {
+      'title': title,
+      // 'address': announcement.location.x + ', ' + announcement.location.y,
+      'price': getRandomArrayElement(prices),
+      'type': getRandomArrayElement(types),
+      'rooms': getRandomArrayElement(rooms),
+      'guests': getRandomArrayElement(guests),
+      'checkin': getRandomArrayElement(checkinArr),
+      'checkout': getRandomArrayElement(checkoutArr),
+      'features': changeArrayLength(features),
+      'description': getRandomArrayElement(descriptions),
+      'photos': changeArrayLength(photos),
     },
-    "location": {
-      "x": getRandomNumber(0, 900),
-      "y": getRandomNumber(130, 630),
+    'location': {
+      'x': getRandomNumber(0, MAP_WIDTH),
+      'y': getRandomNumber(130, 630),
     },
   };
   return announcement;
@@ -90,8 +91,6 @@ var renderMapPin = function (pin) {
   mapPin.setAttribute('style', 'left: ' + pin.location.x + 'px; ' + 'top: ' + pin.location.y + 'px;');
   return mapPin;
 };
-
-
 
 // Наполнение fragment`а DOM - элементами обьявлений
 var fragment = document.createDocumentFragment();
