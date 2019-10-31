75ab5b6aa81f6836d3ef7c64aefbfd59145b456f
UnidentifiedRaccoon <yura.posledov@yandex.ru>
2019-10-31 15:30:26 +0300

    Исправление реализации открытия карточки, упрощение и удаление лишнего кода

--- js/card.js
+++ js/card.js
@@ -27,42 +27,39 @@
     cardTime.textContent = 'Заезд после ' + cardInfo.offer.checkin + ', выезд до ' + cardInfo.offer.checkout;
 
     var cardFeatures = card.querySelector('.popup__features');
-    var featuresFragment = document.createDocumentFragment();
     // Получение содержимого cardFeatures
     var cardFeaturesChildren = cardFeatures.querySelectorAll('li');
+    // Очистка содержимого
+    cardFeatures.textContent = '';
     // Для каждого элемента массива cardFeaturesChildren проводим проверку на содержание класса
     // Один из классов элемента должен иметь любую из приставок перечисленных в массиве cardInfo.offer.features
     for (var i = 0; i < cardInfo.offer.features.length; i++) {
       var featureChild = cardFeaturesChildren[i];
       for (var j = 0; j < cardInfo.offer.features.length; j++) {
         if (featureChild.classList.contains('popup__feature--' + cardInfo.offer.features[j])) {
-          featuresFragment.appendChild(featureChild);
+          // Вставка нужного содержимого
+          cardFeatures.appendChild(featureChild);
         }
       }
     }
-    // Очистка содержимого
-    cardFeatures.textContent = '';
-    // Вставка нужного содержимого
-    cardFeatures.appendChild(featuresFragment);
 
     var cardDescription = card.querySelector('.popup__description');
     cardDescription.textContent = cardInfo.offer.description;
 
 
     var cardPhotos = card.querySelector('.popup__photos');
-    var photosFragment = document.createDocumentFragment();
     // Получение содержимого cardFeatures
     var cardPhotosChild = cardPhotos.querySelector('img');
+    // Очистка содержимого
+    cardPhotos.textContent = '';
     // Клонирование шаблона img, его размножение и вставка в fragment
     for (var z = 0; z < cardInfo.offer.photos.length; z++) {
       var imgChild = cardPhotosChild.cloneNode(true);
       imgChild.src = cardInfo.offer.photos[z];
-      photosFragment.appendChild(imgChild);
+      // Вставка нужного содержимого
+      cardPhotos.appendChild(imgChild);
     }
-    // Очистка содержимого
-    cardPhotos.textContent = '';
-    // Вставка нужного содержимого
-    cardPhotos.appendChild(photosFragment);
+
 
     var cardAvatar = card.querySelector('.popup__avatar');
     cardAvatar.src = cardInfo.author.avatar;
--- js/map.js
+++ js/map.js
@@ -4,47 +4,97 @@
   var ENTER_KEYCODE = 13;
   var ESC_KEYCODE = 27;
 
-  var showCard = function (card) {
-    card.classList.toggle('visually-hidden');
-  };
-  var closeCard = function (card) {
-    card.classList.add('visually-hidden');
+  // Функция показа карточки по клику на пин
+  var showCard = function (pin) {
+    // Сначала закрываем все карточки
+    closeCard();
+    // Затем открываем карточку того пина, на котором произошел клик
+    pin.classList.add('map__pin--active');
+    cardsArr[pin.id].classList.remove('visually-hidden');
+    // Почему нажатие клавиши не срабатывает на карточке? Это может быть связано с отсутствием фокуса на карточке? Как это фиксить и нужно ли?
+    // cardsArr[pin.id].addEventListener('keydown', onCloseXClick)
+    // Добавляем кнопке закрытия карточки обработчик, для закрытия карточки
+    var closeX = cardsArr[pin.id].querySelector('.popup__close');
+    closeX.addEventListener('click', onCloseXClick);
   };
 
-  var addListeners = function (pin, card) {
-    var currenClose = card.querySelector('.popup__close');
-    pin.addEventListener('mousedown', function () {
-      showCard(card);
+  // Функция-Обработчик скрытия карточки
+  var onCloseXClick = function () {
+    closeCard();
+  };
+
+  // Функция скрывающая карточку
+  var closeCard = function () {
+    for (var j = 0; j < pinsArr.length; j++) {
+      // Закрываем все карточки
+      pinsArr[j].classList.remove('map__pin--active');
+      cardsArr[j].classList.add('visually-hidden');
+      // Удаляем обработчик у всех кнопок закрытия карточек
+      var closeX = cardsArr[j].querySelector('.popup__close');
+      closeX.removeEventListener('click', onCloseXClick);
+    }
+  };
+
+  // Функция получениия кнопки(пина) из данных обработчика;
+  var getSelectedPin = function (evt) {
+    var button;
+    // Если мы кликнули на картинку в кнопке, то button присваивается значение родителя картинки
+    // Иначе button присваивается значение кнопки
+    if (evt.path[1].tagName.toLowerCase() === 'button') {
+      button = evt.path[1];
+    } else {
+      button = evt.target;
+    }
+    return button;
+  };
+
+  // Функция добавления обработчиков
+  var addListeners = function () {
+    // Обработчик на map для открытия карточки по клику на пин
+    window.util.map.addEventListener('mousedown', function (evt) {
+      if (evt.target.className === 'map__pin' || evt.path[1].className === 'map__pin') {
+        var selectedPin = getSelectedPin(evt);
+        showCard(selectedPin);
+      }
     });
-    pin.addEventListener('keydown', function (evt) {
+
+    // Обработчик на map для открытия карточки по нажатию на ENTER на пине
+    window.util.map.addEventListener('keydown', function (evt) {
       if (evt.keyCode === ENTER_KEYCODE) {
-        showCard(card);
+        if (evt.target.className === 'map__pin' || evt.path[1].className === 'map__pin') {
+          var selectedPin = getSelectedPin(evt);
+          showCard(selectedPin);
+        }
       }
     });
-    window.addEventListener('keydown', function (evt) {
+
+    // Обработчик на map для закрытия карточки по нажатию на ESC
+    window.util.map.addEventListener('keydown', function (evt) {
       if (evt.keyCode === ESC_KEYCODE) {
-        closeCard(card);
+        closeCard();
       }
-    });
-    currenClose.addEventListener('click', function () {
-      closeCard(card);
     });
   };
 
-  // Наполнение fragment`а DOM - элементами обьявлений
   var pinsFragment = document.createDocumentFragment();
   var cardsFragment = document.createDocumentFragment();
+  var cardsArr = [];
+  var pinsArr = [];
   for (var i = 0; i < window.data.announcements.length; i++) {
-    (function () {
-      var currentPin = window.pin.renderMapPin(window.data.announcements[i]);
-      var currentCard = window.card.renderPinCard(window.data.announcements[i]);
-      // Привязка карточки к пину
-      addListeners(currentPin, currentCard); // Без space
-      // Наполнение fragment`а
-      pinsFragment.appendChild(currentPin);
-      cardsFragment.appendChild(currentCard);
-    })();
+    // Получение DOM объектов пина и карточки
+    var currentPin = window.pin.renderMapPin(window.data.announcements[i]);
+    var currentCard = window.card.renderPinCard(window.data.announcements[i]);
+    // Добавление всем пинам id для связи с карточкой
+    currentPin.id = i;
+    // Создание массивов пинов и карточек (нужно для реализации открытия/закрытия карточки при нажатиии на пин)
+    cardsArr[i] = currentCard;
+    pinsArr[i] = currentPin;
+    // Наполнение fragment`а DOM - элементами обьявлений
+    pinsFragment.appendChild(currentPin);
+    cardsFragment.appendChild(currentCard);
   }
+  // Добавление всех нужных обработчиков
+  addListeners();
 
   window.map = {
     pinsFragment: pinsFragment,
